617e0f5f7dcc563f1f6368175a51237f9dbee57d
ertan-yilzid <ertan.yildiz@laplateforme.io>
2026-04-03 11:43:28 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,12 @@
 chrome.runtime.sendMessage({ type: "GET_SESSION_TIME" }, (r) => {
-  if (r?.success) {
-    window.postMessage({ type: "LOGTIME_SESSION_MINUTES", minutes: r.minutes }, "*");
-  }
+  // Get requiredHours from storage
+  chrome.storage.sync.get({ requiredHours: 30 }, (storage) => {
+    if (r?.success) {
+      window.postMessage({ 
+        type: "LOGTIME_SESSION_MINUTES", 
+        minutes: r.minutes,
+        requiredHours: storage.requiredHours
+      }, "*");
+    }
+  });
 });
